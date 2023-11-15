363b082add70337205a768d5c1113ead6fe366d7
karturbis <vulkanera42@gmx.ch>
2023-11-15 12:35:37 +0100

    add more options to input

--- Main.java
+++ Main.java
@@ -6,38 +6,50 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Choose what you want to do,your options are:\ncreate a new Kangoroo by entering 'k'\nor quit by entering 'q'");
 
-        String option = input.nextLine();
+        Kangoroo kangoroo = null;
+
+        Scanner input = new Scanner(System.in); // opens a scanner so console input can be read.
+
+        System.out.print("Choose what you want to do,your options are:\ncreate a new Kangoroo by entering 'k'\nor quit by entering 'q'\n> ");
+
+        String option = input.nextLine(); // reads the input and saves it into option
 
         
         
 
-        while (true) {
+        while (option.equals("q") == false) {
 
             switch (option) {
 
-            case "k":
-                Kangoroo kangoroo = new Kangoroo();
+                case "k":
+                    kangoroo = new Kangoroo();
 
-            case "q":
-                System.exit(0);
+                case "q":
+                    System.exit(0);
 
-            case "h":
-                System.out.println("");
+                case "h":
+                    System.out.println("help");
 
+                case "r":
 
-            default:
-                System.out.println("The input could not be processed, please try again.");
+                    if(kangoroo != null){
+                        kangoroo.run();
+                    }
+                    else{
+                        System.out.print("You have to create a kangoroo first.");
+                    }
 
+                default:
+                    System.out.println("The input could not be processed, please try again.");
+
+            }
+
+            System.out.print("> ");
+            option = input.nextLine();
             
         }
 
         input.close();
-        
     }
-
-}
-
 }
